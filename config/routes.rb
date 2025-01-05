77e6b9073fdadbc07d480522b74b6763353cef2b
tuananh1611 <le9425399@gmail.com>
2025-01-06 01:25:43 +0700

    dm

--- config/routes.rb
+++ config/routes.rb
@@ -6,23 +6,4 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/products", to: "products#index"
-
-get "/products/new", to: "products#new"
-post "/products", to: "products#create"
-
-get "/products/:id", to: "products#show"
-
-get "/products/:id/edit", to: "products#edit"
-patch "/products/:id", to: "products#update"
-put "/products/:id", to: "products#update"
-
-delete "/products/:id", to: "products#destroy"
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  root "home#index"
 end
