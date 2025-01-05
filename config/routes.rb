41164afce127c04b982877e7b230beb265a596d1
tuananh1611 <le9425399@gmail.com>
2025-01-06 01:12:04 +0700

    dm

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :products
+  root "products#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  get "/products", to: "products#index"
+
+get "/products/new", to: "products#new"
+post "/products", to: "products#create"
+
+get "/products/:id", to: "products#show"
+
+get "/products/:id/edit", to: "products#edit"
+patch "/products/:id", to: "products#update"
+put "/products/:id", to: "products#update"
+
+delete "/products/:id", to: "products#destroy"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
