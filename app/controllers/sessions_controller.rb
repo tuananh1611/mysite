19112d444ee4dff2cfa7c02a982c665f6bcbf005
tuananh1611 <le9425399@gmail.com>
2025-01-06 08:46:48 +0700

    hghh

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def new
+    unless User.exists?(email_address: 'admin@example.com')
+      User.create!(
+        email_address: 'admin@example.com',
+        password: '123'
+        password_confirmation: '123'
+      )
   end
 
   def create
