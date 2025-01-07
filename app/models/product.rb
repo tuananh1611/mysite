4043c7e24e7d93d87ba9b14086ce7708afff03a4
root <root@DESKTOP-VUMKCHF.>
2025-01-08 01:44:04 +0700

    fkfk

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
     # include Notifications
+    has_many :subscribers, dependent: :destroy
     has_one_attached :featured_image
     has_rich_text :description
     validates :name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+
 end
