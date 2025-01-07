868209d05641f5ca6d00167c6f5a659da107f9b0
root <root@DESKTOP-VUMKCHF.>
2025-01-08 02:15:46 +0700

    jdhg

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
-  include Notifications
-  
+ 
+
+  has_many :subscribers, dependent: :destroy
   has_one_attached :featured_image
   has_rich_text :description
-  has_many :subscribers, dependent: :destroy
 
   validates :name, presence: true
   validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
