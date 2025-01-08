5ec5c9e3b9e4dd523c102a0264f8f56eead749d3
root <root@DESKTOP-VUMKCHF.>
2025-01-08 21:39:05 +0700

    jdfj

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "trix"
-pin "@rails/actiontext", to: "actiontext.js"
-pin "trix"
-pin "@rails/actiontext", to: "actiontext.esm.js"
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+
